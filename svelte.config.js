41404ee2e8a121e67aeb961f6fe5557fcf3f81a0
esskayesss <imesskayesss@gmail.com>
2025-08-03 21:19:41 +0530

    specifying node version 22.x

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,16 @@
-import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from "mdsvex";
+import adapter from "@sveltejs/adapter-vercel";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
-	extensions: ['.svelte', '.svx']
+  preprocess: [vitePreprocess(), mdsvex()],
+  kit: {
+    adapter: adapter({
+      runtime: "nodejs22.x",
+    }),
+  },
+  extensions: [".svelte", ".svx"],
 };
 
 export default config;
