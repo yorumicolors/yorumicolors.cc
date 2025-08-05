af4bc8c8f4f88d526318c6b93141c3c04f600136
esskayesss <imesskayesss@gmail.com>
2025-08-06 00:59:44 +0530

    hotfix: invalid themes

--- src/lib/stores/themeStore.ts
+++ src/lib/stores/themeStore.ts
@@ -4,8 +4,16 @@ import { writable } from "svelte/store";
 export type ThemeType = "abyss" | "shade" | "kraken" | "mist";
 export const themeStore = writable<ThemeType>("abyss");
 
+const validThemes = new Set<ThemeType>(["abyss", "shade", "kraken", "mist"]);
+
 if (browser) {
-  themeStore.set((localStorage.getItem("theme") as ThemeType) || "abyss");
+  const storedTheme = localStorage.getItem("theme");
+  if (storedTheme && validThemes.has(storedTheme as ThemeType)) {
+    themeStore.set(storedTheme as ThemeType);
+  } else {
+    themeStore.set("abyss");
+  }
+
   themeStore.subscribe((value) => {
     localStorage.setItem("theme", value);
     document.documentElement.setAttribute("data-theme", value);
